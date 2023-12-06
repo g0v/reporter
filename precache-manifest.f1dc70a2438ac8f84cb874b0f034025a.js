137e8c3106e5560056eaa6445711e9763f63ab59
Lanfon Fan <lanfon.fan@suse.com>
2023-12-07 04:22:20 +0800

    deploy

---
+++ precache-manifest.f1dc70a2438ac8f84cb874b0f034025a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd912ba6ed27507b5603",
+    "url": "/reporter/css/app.d60b10c3.css"
+  },
+  {
+    "revision": "3577b562860669083732890413fd41c9",
+    "url": "/reporter/g0v.json"
+  },
+  {
+    "revision": "11d28aafbad99d9168517daaa365ae21",
+    "url": "/reporter/images/og.png"
+  },
+  {
+    "revision": "df0213a7cae0495bc8d285ee081eacf4",
+    "url": "/reporter/images/r2.png"
+  },
+  {
+    "revision": "e2e947462c44a18eabd39b1e6ae10e94",
+    "url": "/reporter/index.html"
+  },
+  {
+    "revision": "437d721e7afcfad836c5",
+    "url": "/reporter/js/about.68cec688.js"
+  },
+  {
+    "revision": "bd912ba6ed27507b5603",
+    "url": "/reporter/js/app.cefc0f41.js"
+  },
+  {
+    "revision": "53f60ff48421c1d9b40a",
+    "url": "/reporter/js/chunk-vendors.90c92bdf.js"
+  },
+  {
+    "revision": "20e3b7fee58ddce319631bc3e35e987a",
+    "url": "/reporter/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/reporter/robots.txt"
+  }
+]);
